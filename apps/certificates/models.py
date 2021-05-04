db76551f10dc661642b11447c93371d4a338223a
muturi254 <petermuturi74@gmail.com>
2021-05-04 17:14:38 +0300

    add bug fix

--- apps/certificates/models.py
+++ apps/certificates/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 from apps.utils.models import Timestamps
 # Create your models here.
-class Certificate(models.Model, Timestamps):
+class Certificate(Timestamps, models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(max_length=200)
+    description = models.TextField(max_length=250)
