6e706f99a04b34446acd46085bf59f4b86e4c69b
muturi254 <petermuturi74@gmail.com>
2021-05-04 17:50:58 +0300

    customize admin display for cerificate model

--- apps/certificates/admin.py
+++ apps/certificates/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from apps.certificates.models import Certificate
 # Register your models here.
+
+class CertficateAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_at', 'updated_at']
+    search_field = ['name']
+    list_filter = ['name']
+
+
+
+admin.site.register(Certificate, CertficateAdmin)
--- apps/certificates/models.py
+++ apps/certificates/models.py
@@ -5,3 +5,6 @@ from apps.utils.models import Timestamps
 class Certificate(Timestamps, models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.name
