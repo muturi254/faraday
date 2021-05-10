5ae6f6f909eeefca3164d60f21a7df10d88f42d6
muturi254 <petermuturi74@gmail.com>
2021-05-10 23:55:03 +0300

    add jwt token views

--- schoolApi/settings.py
+++ schoolApi/settings.py
@@ -132,6 +132,6 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simpejwt.authentication.JWTAuthentication'
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
     )
 }
--- schoolApi/urls.py
+++ schoolApi/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/users/', include("apps.users.urls")),
     path('api/v1/certificates/', include("apps.certificates.urls")),
     path('api/v1/lectures/', include("apps.lectures.urls")),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
