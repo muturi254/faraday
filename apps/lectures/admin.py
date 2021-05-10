a4f86b0557fad40f4f7f80995c4c79a177186437
muturi254 <petermuturi74@gmail.com>
2021-05-10 17:21:03 +0300

    register models to admin

--- apps/lectures/admin.py
+++ apps/lectures/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from apps.lectures.models import Lecture
+
 # Register your models here.
+class LectureAdmin(admin.ModelAdmin):
+    list_display = ['title', 'lecturer_name', 'date', 'is_required']
+    search_fields = ['title', 'lecturer_name']
+
+admin.site.register(Lecture, LectureAdmin)
--- apps/lectures/models.py
+++ apps/lectures/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 from apps.utils.models import Timestamps
 # Create your models here.
@@ -6,5 +7,12 @@ class Lecture(Timestamps, models.Model):
     title = models.CharField(max_length=100)
     lecturer_name = models.CharField(max_length=100, blank=True, default='')
     description = models.TextField()
+    duration = models.IntegerField(validators=[
+        MinValueValidator(1), MaxValueValidator(10)
+    ], help_text='Enter n.o of hours')
     slides_url = models.CharField(max_length=255)
     date = models.DateTimeField()
+    is_required = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
