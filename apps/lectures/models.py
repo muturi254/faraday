827a958f1f8a6b115721c4e98ed35f0a86c35fb2
muturi254 <petermuturi74@gmail.com>
2021-05-10 16:58:36 +0300

    register lecture app in project settings

--- apps/lectures/models.py
+++ apps/lectures/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+from apps.utils.models import Timestamps
 # Create your models here.
+class Lecture(Timestamps, models.Model):
+    title = models.CharField(max_length=100)
+    lecturer_name = models.CharField(max_length=100, blank=True, default='')
+    description = models.TextField()
+    slides_url = models.CharField(max_length=255)
+    date = models.DateTimeField()
