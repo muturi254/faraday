759c5aaf238692c95e9794be42d72e9f318dda29
muturi254 <petermuturi74@gmail.com>
2021-05-10 12:53:56 +0300

    models fix

--- apps/waitlist/models.py
+++ apps/waitlist/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from apps.utils.models import Timestamps
+
 # Create your models here.
+class WaitListEntry(Timestamps, models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True
+    )
+    notes = models.TextField()
